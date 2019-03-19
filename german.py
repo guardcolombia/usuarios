56c68fb9666d8dfcb6c69ce41d5cd4d64dbad820
BGHTOKEN <31333238+guardcolombia@users.noreply.github.com>
2019-03-19 05:46:53 +0000

    Add files via upload

---
+++ german.py
@@ -0,0 +1,63 @@
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError
+import sys
+import csv
+import random
+import time
+api_id = 623438
+api_hash = '50c964e471536b4981ec5b608a85034b'
+phone = '+573013307466'
+
+SLEEP_TIME = 60
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+input_file = sys.argv[1]
+users = []
+with open(input_file, encoding='UTF-8') as f:
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = {}
+        user['username'] = row[0]
+        user['id'] = int(row[1])
+        user['access_hash'] = int(row[2])
+        user['name'] = row[3]
+        users.append(user)
+
+mode = int(input("Enter 1 to send by user ID or 2 to send by username: "))
+
+messages= ["Hello {},I want to invite you to my OTC bitcoin Group, We trade BTC,USDT,ETH,ETC Escrow services, Members with 10,000 Positive trades in localbitcoins : (Legacy, bitgo, Tmf )or multiple signature bitpay Wallet, we sell/buy in everywhere Link :http://bit.ly/otcmarkets,Please Dont report as SPAM, just say DONT DISTRUB, and ill never send this message"]
+
+for user in users:
+    if mode == 2:
+        if user['username'] == "":
+            continue
+        receiver = client.get_input_entity(user['username'])
+    elif mode == 1:
+        receiver = InputPeerUser(user['id'],user['access_hash'])
+    else:
+        print("Invalid Mode. Exiting.")
+        client.disconnect()
+        sys.exit()
+    message = random.choice(messages)
+    try:
+        print("Sending Message to:", user['name'])
+        client.send_message(receiver, message.format(user['name']))
+        print("Waiting {} seconds".format(SLEEP_TIME))
+        time.sleep(SLEEP_TIME)
+    except PeerFloodError:
+        print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+        client.disconnect()
+        sys.exit()
+    except Exception as e:
+        print("Error:", e)
+        print("Trying to continue...")
+        continue
+client.disconnect()
+print("Done. Message sent to all users.")
